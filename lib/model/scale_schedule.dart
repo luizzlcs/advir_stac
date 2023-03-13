ddf63470bb016d16ae50294f768e2a70be8fada7
Luiz Carlos <luizzlcs@hotmail.com>
2023-03-13 19:08:59 -0300

    create metodo to generate Schedule

--- lib/model/scale_schedule.dart
+++ lib/model/scale_schedule.dart
@@ -16,4 +16,25 @@ class ScaleSchedule {
     required this.dayDescription,
     this.situation = Status.ACTIVE,
   });
+
+  void generateSchedule(int month) {
+    DateTime startDate = DateTime(DateTime.now().year, month,
+        1); // Define a data de início como o primeiro dia do mês especificado
+    int daysInMonth = DateTime(DateTime.now().year, month + 1, 0)
+        .day; // Obtém o número de dias no mês especificado
+
+    for (int i = 0; i < daysInMonth; i++) {
+      DateTime currentDate = startDate.add(Duration(days: i));
+      if (currentDate.weekday == DateTime.sunday ||
+          currentDate.weekday == DateTime.wednesday ||
+          currentDate.weekday == DateTime.saturday) {
+        print(
+            'Agendar para ${currentDate.day}/${currentDate.month}/${currentDate.year}:');
+        print(' - Domingo');
+        print(' - Quarta-feira');
+        print(' - Sábado');
+        print('---');
+      }
+    }
+  }
 }
